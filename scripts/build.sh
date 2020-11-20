fa6fd0641f2fe7e9a44260a6d9c6958b1b38373a
Olivier Levitt <ungawa14@gmail.com>
2020-11-20 18:40:21 +0100

    Simplify CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,16 +8,9 @@ echo "Building the universe!"
 
 mkdir -p ${REPO_BASE_DIR}/target/
 
-# Create a new virtual environment
-rm -rf ${REPO_BASE_DIR}/target/venv
-python3 -m venv ${REPO_BASE_DIR}/target/venv
-
 # Install dependencies
 pip3 install -r ${SCRIPTS_DIR}/requirements/requirements.txt
 
-"${REPO_BASE_DIR}"/target/venv/bin/python3 "$SCRIPTS_DIR"/validate_packages.py
-"${REPO_BASE_DIR}"/target/venv/bin/python3 "$SCRIPTS_DIR"/gen_universe.py \
-  --repository="${REPO_BASE_DIR}"/repo/packages/ --out-dir="${REPO_BASE_DIR}"/target/
+python3 "$SCRIPTS_DIR"/validate_packages.py
+python3 "$SCRIPTS_DIR"/gen_universe.py --repository="${REPO_BASE_DIR}"/repo/packages/ --out-dir="${REPO_BASE_DIR}"/target/
 
-# Delete virtual environment
-rm -rf ${REPO_BASE_DIR}/target/venv
